b482f3187a13f79d20edd805149479da024a0383
MatheusLG <135284500+MatheusLG@users.noreply.github.com>
2025-08-14 18:23:55 +0000

    Primeiros testes de Interface

---
+++ main.js
@@ -0,0 +1,34 @@
+/*function soma(){
+
+}
+
+function sub(){
+
+}
+*/
+function mult(){
+    var exemplo = new String="2 x 2";
+    eval(exemplo.toString());
+    console.log(exemplo);
+}
+
+/*function div(){
+
+}
+
+function cleanAll(){
+
+}
+
+function clean1carac(){
+
+}
+
+function porcent(){
+
+}
+
+function resul(){
+
+}
+*/
